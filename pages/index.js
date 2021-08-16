d9131e90a69ea3548598cc425c19b10f391c347f
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-08-16 15:23:30 -0400

    Static Props Home

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
 
-export default function Home() {
-  return <h1>Hola</h1>;
+export default function Home({ posts }) {
+  return (
+    <>
+      <h1>Hello</h1>
+      {posts.map((p, i) => (
+        <h2 key={i}>{p.frontmatter.title}</h2>
+      ))}
+    </>
+  );
 }
+
+export const getStaticProps = () => {
+  // Leer los archivos de la carpeta posts
+  const files = fs.readdirSync(path.join("posts"));
+
+  // Obtener el slug y frontmatter de los posts
+  const posts = files.map((f) => {
+    // Crear el slug
+    const slug = f.replace(".md", "");
+
+    // Obtener el frontmatter
+    const markdownWithMeta = fs.readFileSync(path.join("posts", f), "utf-8");
+
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return {
+      slug,
+      frontmatter,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
---
+++ posts/test.md
@@ -0,0 +1,8 @@
+---
+title: 'Test Post'
+date: 'August 16, 2021'
+excerpt: 'A very basic post'
+cover_image: '/images/posts/img3.jpg'
+---
+
+I really don't know what I want to do with this.
