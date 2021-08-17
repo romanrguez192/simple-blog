e1c0430d40cefb08cd915db4a8fe5d986a472517
Román Rodríguez <rdrodriguez.19@est.ucab.edu.ve>
2021-08-16 22:32:59 -0400

    Limpiando

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
 import Meta from "../components/Meta";
 import Header from "../components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <>
       <Meta />
       <Header />
       <Component {...pageProps} />
-    </ChakraProvider>
+    </>
   );
 }
 
--- pages/posts/[slug].jsx
+++ pages/posts/[slug].jsx
@@ -3,38 +3,12 @@ import path from "path";
 import matter from "gray-matter";
 import marked from "marked";
 import Meta from "../../components/Meta";
-import styles from "../../styles/Post.module.css";
-import ReactMarkdown from "react-markdown";
-import { Heading } from "@chakra-ui/react";
 
 export default function Post({ title, date, cover_image, excerpt, markdown }) {
   return (
     <>
       <Meta title={title} description={excerpt} />
-      {/* <h1>A title</h1>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <h2>A title</h2>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <h3>A title</h3>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <ol>
-        <li>A title</li>
-        <li>A second title</li>
-        <li>A second title</li>
-        <li>A second title</li>
-      </ol>
-      <ul>
-        <li>A title</li>
-        <li>A second title</li>
-        <li>A second title</li>
-        <li>A second title</li>
-      </ul>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-      <p>Some text which is really long and bla bla bla aaaaaaaa i don know</p>
-     */}
-    <ReactMarkdown children={markdown} />
+
     </>
   );
 }
